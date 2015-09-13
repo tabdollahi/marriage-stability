1efc6e6dbb1b15a710e9a9d7ccc7b754248ea214
tabdollahi <abdollahi.tania@gmail.com>
2015-09-13 19:14:22 -0400

    edit display functionality

--- marriage_stability.rb
+++ marriage_stability.rb
@@ -11,35 +11,44 @@ class StableMatching
   end
 
   def dating_game
-    while @initiators.any? { |initiator| initiator.single? }
+    while any_singles?
       play_round
+      display_marital_statuses
     end
   end
 
   def play_round
-    @initiators.select {|x| x.single?}.each do |current_suitor|
+    single_persons(@initiators).each do |current_suitor|
       current_suitor.propose_to_next_preferred
     end
   end
 
   def display_preferences
-    display_helper(@initiators)
-    display_helper(@receivers)
+    display_preferences_helper(@initiators)
+    display_preferences_helper(@receivers)
   end
 
-  def display_engagements
+  def display_marital_statuses
     puts "*"*80
-    puts "Engagements"
-    @receivers.each do |receiver|
-      if receiver.fiance != nil
-        puts "#{receiver.name}: #{receiver.fiance.name}"
-      end
+    puts "Marital Statuses"
+    puts "*"*80
+    @receivers.select {|x| !x.single?}.each do |receiver|
+      puts "#{receiver.name}: #{receiver.fiance.name}"
     end
+    display_singles
   end
 
   private
 
-  def display_helper(group)
+  def any_singles?
+    @initiators.any? { |initiator| initiator.single? }
+  end
+
+  def single_persons(group)
+    group.select {|x| x.single?}
+  end
+
+  def display_preferences_helper(group)
     group.each do |person|
       puts "These are #{person.name}'s preferences:"
       person.preferences.each {|person| puts person.name}
@@ -47,13 +56,26 @@ class StableMatching
     end
   end
 
+  def display_singles_helper(group)
+    single_persons(group).each do |person|
+      puts "#{person.name}"
+    end
+  end
+
+  def display_singles
+    if any_singles?
+      puts "Singles:"
+      display_singles_helper(@initiators)
+      display_singles_helper(@receivers)
+    end
+  end
 end
 
 
 
-# test = StableMatching.new(RandomPersonsGenerator.new(4))
+test = StableMatching.new(RandomPersonsGenerator.new(4))
 
 
-# test.display_preferences
-# test.dating_game
-# test.display_engagements
+test.display_preferences
+test.dating_game
+
